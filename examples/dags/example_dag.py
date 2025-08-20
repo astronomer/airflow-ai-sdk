77ab352f62ab71bc311560d80c9a841023e2b163
Bas Harenslak <bas@astronomer.io>
2025-08-20 12:11:28 +0200

    Formatting

--- examples/dags/example_dag.py
+++ examples/dags/example_dag.py
@@ -1,11 +1,7 @@
 from airflow.decorators import dag, task
 
 
-@task.llm(
-    model="gpt-4o-mini",
-    system_prompt="Do some robot stuff",
-    output_type=str
-)
+@task.llm(model="gpt-4o-mini", system_prompt="Do some robot stuff", output_type=str)
 def do_stuff() -> str:
     return "do stuff"
 
